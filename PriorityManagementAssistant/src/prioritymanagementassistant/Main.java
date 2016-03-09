99a1bb512c914b275fe61ea966fd2fc402778135
abraunag <abraunag@oswego.edu>
2016-03-08 20:32:10 -0500

    Edited Souts, Compared days to months

--- PriorityManagementAssistant/src/prioritymanagementassistant/Main.java
+++ PriorityManagementAssistant/src/prioritymanagementassistant/Main.java
@@ -20,7 +20,7 @@ enable screen pop-ups as reminders for those assignments.
 
 
 
-package prioritymanagementassistant;
+package main;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -64,7 +64,7 @@ public class Main {
             String build;
             task = getTask();
             month = getMonth();
-            day = getDay();
+            day = getDay2(month);
             year = getYear();
             hour = getHour();
             minute = getMinute();
@@ -102,21 +102,84 @@ public class Main {
         }
         private static int getMonth(){
             do {
-                System.out.println("Please Enter The Month Of Your Task's Due Date\t" + ("MM"));  //prompt user for month
+                System.out.println("Please Enter The Month Of Your Task's Due Date\t(1-12)");  //prompt user for month
                 line = kb.nextLine();
                 
                 try {   //promt the user for an integer number
                     numberFlag = false;
                     month = Integer.parseInt(line);
                     badInfoFlag = false;
-                    if(month < 1 || month > 12)
-                        badInfoFlag = true;
+                    if(month < 1 || month > 12){
+                       System.out.println(month + " is not a valid Month, please enter a number between 1 and 12.");
+                       badInfoFlag = true;
+                    }
                 } catch (NumberFormatException e) {
                     numberFlag = true;
                 }
             } while (line == null || numberFlag || badInfoFlag); //prompt the user for no input
             return month;
         }
+        
+        private static int getDay2(int m){
+            if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12 ){
+               do{
+                   System.out.println("Please Enter The Day Of Your Task's Due Date\t(1-31)");
+                   line = kb.nextLine();
+                   try {   //promt the user for an integer number
+                        numberFlag = false;
+                        day = Integer.parseInt(line);
+                        badInfoFlag = false;
+                        if(day < 1 || day > 31){
+                            System.out.println(day + " is not a valid Day, please enter a number between 1 and 31.");
+                            badInfoFlag = true;
+                        }
+                    }catch (NumberFormatException e) {
+                        numberFlag = true;
+                }
+               }while(line == null || numberFlag || badInfoFlag);
+                   return day;
+            }else if(m == 4 || m == 6 || m == 9 || m == 11){
+               do{
+                   System.out.println("Please Enter The Day Of Your Task's Due Date\t(1-30)");
+                   line = kb.nextLine();
+                   try {   //promt the user for an integer number
+                        numberFlag = false;
+                        day = Integer.parseInt(line);
+                        badInfoFlag = false;
+                        if(day < 1 || day > 30){
+                            System.out.println(day + " is not a valid Day, please enter a number between 1 and 30.");
+                            badInfoFlag = true;
+                        }
+                    }catch (NumberFormatException e) {
+                        numberFlag = true;
+                    }
+               }while(line == null || numberFlag || badInfoFlag);
+                   return day;                 
+            }else if(month == 2){
+               do{
+                   System.out.println("Please Enter The Day Of Your Task's Due Date\t(1-31)");
+                   line = kb.nextLine();
+                   try {   //promt the user for an integer number
+                        numberFlag = false;
+                        day = Integer.parseInt(line);
+                        badInfoFlag = false;
+                        if(day < 1 || day > 28){
+                            System.out.println(day + " is not a valid Day, please enter a number between 1 and 28.");
+                            badInfoFlag = true;
+                        }
+                    }catch (NumberFormatException e) {
+                        numberFlag = true;
+                    }
+               }while(line == null || numberFlag || badInfoFlag);
+                   return day;                 
+            }
+        
+            return 0;
+        }   
+        
+      
+        
+       
         private static int getDay(){
             do {
                 System.out.println("Please Enter The Day Of Your Task's Due Date\t" + ("DD"));  //prompt user for day
@@ -143,8 +206,10 @@ public class Main {
                     numberFlag = false;
                     year = Integer.parseInt(line);
                     badInfoFlag = false;
-                    if(year < 2016)
+                    if(year < 2016){
+                        System.out.println(year + " is not a valid Year, please enter a number greater than 2015.");
                         badInfoFlag = true;
+                    }
                 } catch (NumberFormatException e) {
                     numberFlag = true;
                 }
@@ -160,8 +225,10 @@ public class Main {
                     numberFlag = false;
                     hour = Integer.parseInt(line);
                     badInfoFlag = false;
-                    if(hour < 1 || hour > 24)
+                    if(hour < 1 || hour > 24){
+                        System.out.println(hour + " is not a valid Hour, please enter a number between 1 and 24.");
                         badInfoFlag = true;
+                    }
                 } catch (NumberFormatException e) {
                     numberFlag = true;
                 }
@@ -177,8 +244,10 @@ public class Main {
                     numberFlag = false;
                     minute = Integer.parseInt(line);
                     badInfoFlag = false;
-                    if(minute < 0 || minute > 59)
+                    if(minute < 0 || minute > 59){
+                        System.out.println(minute + " is not a valid Minute, please enter a number between 1 and 60.");
                         badInfoFlag = true;
+                    }
                 } catch (NumberFormatException e) {
                     numberFlag = true;
                 }
@@ -197,8 +266,10 @@ public class Main {
                         numberFlag = false;
                         priority = Integer.parseInt(line);
                         badInfoFlag = false;
-                        if(priority < 1 || priority > 3)
+                        if(priority < 1 || priority > 3){
+                            System.out.println(priority + " is not a valid Priority, please enter a number between 1 and 3.");
                             badInfoFlag = true;
+                        }
                     } catch (NumberFormatException e) {
                         numberFlag = true;
                     }
